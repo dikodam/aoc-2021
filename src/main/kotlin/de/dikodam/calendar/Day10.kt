3b4f17e211b2e3ef5a1caeaedf69dd9f358ac51d
adiko <dikodam@googlemail.com>
2021-12-09 09:29:03 +0100

    day 10 setup

---
+++ src/main/kotlin/de/dikodam/calendar/Day10.kt
@@ -0,0 +1,20 @@
+package de.dikodam.calendar
+
+import de.dikodam.AbstractDay
+import de.dikodam.executeTasks
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+fun main() {
+    Day10().executeTasks()
+}
+
+class Day10 : AbstractDay() {
+    override fun task1(): String {
+        return ""
+    }
+
+    override fun task2(): String {
+        return ""
+    }
+}
