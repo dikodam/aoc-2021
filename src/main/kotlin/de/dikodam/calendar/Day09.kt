d73e5c46289c0591ac77b12cf721ad908dedeb54
dikodam <adiko@gmx.de>
2021-12-09 09:02:52 +0100

    day 09 task 2 bfs but its broken

--- src/main/kotlin/de/dikodam/calendar/Day09.kt
+++ src/main/kotlin/de/dikodam/calendar/Day09.kt
@@ -3,6 +3,7 @@ package de.dikodam.calendar
 import de.dikodam.AbstractDay
 import de.dikodam.Coordinates2D
 import de.dikodam.executeTasks
+import java.util.*
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
@@ -36,8 +37,60 @@ class Day09 : AbstractDay() {
             .mapNotNull { map[it] }
     }
 
-    override fun task2(): String {
-        return ""
+    fun getNeighborsCoords(point: Coordinates2D, map: Map<Coordinates2D, Int>): List<Coordinates2D> {
+        val up = point.copy(x = point.x - 1)
+        val down = point.copy(x = point.x + 1)
+        val left = point.copy(y = point.y - 1)
+        val right = point.copy(y = point.y + 1)
+        return listOf(up, down, left, right)
+            .filter { spot -> spot in map.keys }
     }
 
+    override fun task2(): String {
+        // bfs
+        // for each point,
+        //  if registered, skip
+        //  if not registered, register
+        //   if ==9 skip else start new basin:
+        // getNeighbors
+        // filterNonRegistered Neighbors
+        // register each != 9 as part current basin
+        // flatMap each != 9 neighbor to its neighbors
+        // when done
+
+        val basins = mutableListOf<Basin>()
+        val visitedPoints = mutableSetOf<Coordinates2D>()
+        for (point in input.keys) {
+            if (point in visitedPoints) continue
+            visitedPoints += point
+            if (input[point]!! == 9) continue
+            val currentBasin = mutableSetOf<Coordinates2D>()
+
+            val stack = ArrayDeque<Coordinates2D>()
+            stack += getNeighborsCoords(point, input)
+
+            while (stack.isNotEmpty()) {
+                val currentPoint = stack.remove()
+                if (visitedPoints.contains(currentPoint)) {
+                    continue
+                } else {
+                    visitedPoints += currentPoint
+                }
+                if (input[currentPoint] == 9) {
+                    continue
+                }
+                currentBasin += currentPoint
+                stack += getNeighborsCoords(currentPoint, input)
+            }
+            basins.add(currentBasin)
+        }
+
+        val result = basins.map { it.size }
+            .sortedDescending()
+            .take(3)
+            .reduce(Int::times)
+        return "$result"
+    }
 }
+
+typealias Basin = Set<Coordinates2D>
